7d4c9984d5dedf7c4daa5cdda9a09dbaf6b3400a
mujica1301 <andresemiliomujica2006@gmail.com>
2025-11-22 17:17:01 -0400

    Nodo y Lista Enlazada 2

--- src/supermetromendeley/ListaEnlazada.java
+++ src/supermetromendeley/ListaEnlazada.java
@@ -3,11 +3,103 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package supermetromendeley;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- *
+ * Lista enlazada como reemplazo de ArrayList.
  * @author NITRO
  */
-public class ListaEnlazada {
+public class ListaEnlazada<T> implements Iterable<T> {
+    private Nodo<T> head;
+    private int size;
     
+    public ListaEnlazada(){
+        head = null;
+        size = 0;
+    }
+    
+    public void agregar(T elemento) {
+        Nodo<T> nuevo = new Nodo<>(elemento);
+        if (head == null){
+            head = nuevo;
+        } else {
+            Nodo<T> actual = head;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+        size++;
+    }
+    
+    
+    
+    public T obtener(int indice) {
+        validarIndice(indice);
+        Nodo<T> actual = head;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.siguiente;
+        }
+        return actual.dato;
+    }
+    
+    public boolean contiene(T elemento) {
+        for (T dato : this){
+            if (dato.equals(elemento)) return true;
+        }
+        return false;
+    }
+    
+    public boolean eliminar(T elemento) {
+        if (head == null) return false;
+        if (head.dato.equals(elemento)) {
+            head = head.siguiente;
+            size--;
+        }
+        Nodo<T> actual = head;
+        while (actual.siguiente != null) {
+            if (actual.siguiente.dato.equals(elemento)) {
+                actual.siguiente = actual.siguiente.siguiente;
+                size--;
+                return true;
+            }
+            actual = actual.siguiente;
+        }
+        return false;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public boolean estaVacia() {
+        return size == 0;
+    }
+    
+    private void validarIndice(int indice) {
+        if (indice < 0 || indice >= size) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango: " + indice);
+        }
+    }
+    
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Nodo<T> actual = head;
+            
+            @Override
+            public boolean hasNext() {
+                return actual != null;
+            }
+            
+            @Override
+            public T next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                T dato = actual.dato;
+                actual = actual.siguiente;
+                return dato;
+            }
+        };
+    }
 }
--- src/supermetromendeley/Nodo.java
+++ src/supermetromendeley/Nodo.java
@@ -5,9 +5,27 @@
 package supermetromendeley;
 
 /**
- *
+ * Nodo genérico para ListaEnlazada.
  * @author NITRO
  */
-public class Nodo {
-    
+public class Nodo<T> {
+    T dato;
+    Nodo<T> siguiente;
+
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
 }
